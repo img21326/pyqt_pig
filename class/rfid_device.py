ebeb9611ce911d0626a8a06603ac5c1c284fd984
img21326 <img21326@gmail.com>
2020-04-09 11:03:25 +0800

    [class] add rfid device init

---
+++ class/rfid_device.py
@@ -0,0 +1,54 @@
+import serial
+import time
+class RFID():
+    ser = None
+
+    VER_COMMAND = [0x02,0xA4]
+    READ_COMMAND = b'550D'
+
+    def __init__(self,port = None):
+        if port == None:
+            port = "/dev/tty.usbmodem201201011"
+        
+        try:
+            self.ser = serial.Serial(port)
+            print(self.ser)
+        except Exception as e:
+            print("connect port error:")
+            print(e)
+            exit()
+    
+    def get_version(self):
+        r = self.write(self.VER_COMMAND)
+        return r
+
+    def get_card(self):
+        r = self.write(self.READ_COMMAND)
+        return r
+        
+
+    def write(self,command):
+        self.ser.write(command)
+        time.sleep(1)
+        r = []
+        while self.ser.in_waiting:
+            r.append(self.ser.read())
+        return r
+
+    def close(self):
+        self.ser.close()
+
+    @staticmethod
+    def get_instance(port = None):
+        rfid = RFID(port)
+        return rfid
+
+if __name__ == "__main__":
+    rfid = RFID.get_instance()
+    print("RFID VERSION:")
+    print(rfid.get_version())
+
+    print("RFID READ CARD:")
+    print(rfid.get_card()) 
+
+    rfid.close()

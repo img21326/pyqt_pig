2a4423d316fe1c1a568794e45893bd0ae59a1131
Andrew Liao <img21326@gmail.com>
2020-05-25 09:27:37 +0800

    add water ui

--- weight_ui.py
+++ weight_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '.\weight.ui'
+# Form implementation generated from reading ui file 'weight.ui'
 #
 # Created by: PyQt5 UI code generator 5.12
 #
@@ -12,7 +12,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(766, 279)
+        MainWindow.resize(767, 551)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
@@ -60,6 +60,51 @@ class Ui_MainWindow(object):
         self.tableView = QtWidgets.QTableView(self.centralwidget)
         self.tableView.setGeometry(QtCore.QRect(310, 20, 441, 231))
         self.tableView.setObjectName("tableView")
+        self.tableView_2 = QtWidgets.QTableView(self.centralwidget)
+        self.tableView_2.setGeometry(QtCore.QRect(310, 280, 441, 231))
+        self.tableView_2.setObjectName("tableView_2")
+        self.groupBox_3 = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox_3.setGeometry(QtCore.QRect(10, 400, 291, 111))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(16)
+        self.groupBox_3.setFont(font)
+        self.groupBox_3.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.groupBox_3.setObjectName("groupBox_3")
+        self.label_4 = QtWidgets.QLabel(self.groupBox_3)
+        self.label_4.setGeometry(QtCore.QRect(20, 40, 111, 31))
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(self.groupBox_3)
+        self.label_5.setGeometry(QtCore.QRect(20, 70, 81, 41))
+        self.label_5.setObjectName("label_5")
+        self.label_weight_datetime_2 = QtWidgets.QLabel(self.groupBox_3)
+        self.label_weight_datetime_2.setGeometry(QtCore.QRect(120, 40, 161, 31))
+        self.label_weight_datetime_2.setObjectName("label_weight_datetime_2")
+        self.label_weight_value_2 = QtWidgets.QLabel(self.groupBox_3)
+        self.label_weight_value_2.setGeometry(QtCore.QRect(120, 80, 161, 21))
+        self.label_weight_value_2.setObjectName("label_weight_value_2")
+        self.groupBox_4 = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox_4.setGeometry(QtCore.QRect(10, 280, 291, 111))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(16)
+        self.groupBox_4.setFont(font)
+        self.groupBox_4.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.groupBox_4.setStyleSheet("")
+        self.groupBox_4.setObjectName("groupBox_4")
+        self.label_8 = QtWidgets.QLabel(self.groupBox_4)
+        self.label_8.setGeometry(QtCore.QRect(20, 30, 81, 41))
+        self.label_8.setObjectName("label_8")
+        self.label_rfid_value_3 = QtWidgets.QLabel(self.groupBox_4)
+        self.label_rfid_value_3.setGeometry(QtCore.QRect(60, 40, 221, 21))
+        self.label_rfid_value_3.setObjectName("label_rfid_value_3")
+        self.label_9 = QtWidgets.QLabel(self.groupBox_4)
+        self.label_9.setGeometry(QtCore.QRect(20, 70, 121, 41))
+        self.label_9.setObjectName("label_9")
+        self.label_rfid_value_4 = QtWidgets.QLabel(self.groupBox_4)
+        self.label_rfid_value_4.setGeometry(QtCore.QRect(140, 80, 161, 21))
+        self.label_rfid_value_4.setText("")
+        self.label_rfid_value_4.setObjectName("label_rfid_value_4")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -71,7 +116,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.groupBox.setTitle(_translate("MainWindow", "秤重儀"))
+        self.groupBox.setTitle(_translate("MainWindow", "Food"))
         self.label_2.setText(_translate("MainWindow", "Datetime:"))
         self.label_3.setText(_translate("MainWindow", "Value:"))
         self.label_weight_datetime.setText(_translate("MainWindow", "Waiting"))
@@ -80,5 +125,14 @@ class Ui_MainWindow(object):
         self.label_6.setText(_translate("MainWindow", "UID:"))
         self.label_rfid_value.setText(_translate("MainWindow", "Waiting"))
         self.label_7.setText(_translate("MainWindow", "CountTime:"))
+        self.groupBox_3.setTitle(_translate("MainWindow", "Water"))
+        self.label_4.setText(_translate("MainWindow", "Datetime:"))
+        self.label_5.setText(_translate("MainWindow", "Value:"))
+        self.label_weight_datetime_2.setText(_translate("MainWindow", "Waiting"))
+        self.label_weight_value_2.setText(_translate("MainWindow", "Waiting"))
+        self.groupBox_4.setTitle(_translate("MainWindow", "RFID"))
+        self.label_8.setText(_translate("MainWindow", "UID:"))
+        self.label_rfid_value_3.setText(_translate("MainWindow", "Waiting"))
+        self.label_9.setText(_translate("MainWindow", "CountTime:"))
 
 
